a2d70197bb4e6b0154f194830ddb407730e0aba6
RVanathi <vanathisulochana@gmail.com>
2023-04-13 07:15:27 +0530

    Fix flex-row elements overlapping in About section

--- components/About.tsx
+++ components/About.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import AboutImage from "../public/assets/bitmoji.jpg";
+import AboutImage from "../public/assets/coffee.jpg";
 
 type Props = {};
 
 const About = (props: Props) => {
   return (
     <motion.div
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-4xl px-10 justify-evenly mx-auto items-center"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-[#1f7b70] text-2xl">
         About
@@ -26,13 +26,17 @@ const About = (props: Props) => {
         }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        className="relative -mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+        className=""
       >
-        <Image className="rounded-full " alt="" src={AboutImage} />
+        <Image
+          className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-fit md:rounded-lg md:w-[450px] md:h-[280px] xl:w-[600px] xl:h-[350px]"
+          alt=""
+          src={AboutImage}
+        />
       </motion.div>
 
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl">
+      <div className="space-y-10 px-0 md:px-10 w-fit">
+        <h4 className="text-3xl">
           Here is a{" "}
           <span className="underline decoration-[#A1C181]/50">little</span>{" "}
           background
@@ -41,13 +45,13 @@ const About = (props: Props) => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempora
           saepe itaque quisquam animi sint rerum, suscipit quas ullam vel eum,
           vitae obcaecati! Ea, assumenda impedit corporis inventore asperiores
-          quo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          iusto obcaecati vero dolore commodi explicabo quo, totam laborum quia,
-          doloremque neque. Recusandae asperiores velit amet aliquam minus
-          voluptate rerum itaque! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Eveniet veniam ut vero quisquam architecto, quidem
-          similique soluta porro, corrupti eius reprehenderit deleniti velit
-          dicta veritatis consequatur laborum? Magni, odio consequatur!
+          quo. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Quibusdam hic aut vero quas odio earum assumenda possimus. Cumque,
+          voluptates, perspiciatis saepe, ratione expedita unde amet doloribus
+          eligendi ad sit nemo! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Sapiente quae dignissimos, modi asperiores provident
+          repellat debitis accusantium animi natus quidem molestias, quo totam
+          similique facilis assumenda cum sed corrupti quam.
         </p>
       </div>
     </motion.div>
