0250700242a7ce9072701c6301b2f90c580febcd
RVanathi <vanathisulochana@gmail.com>
2023-06-08 09:32:33 +0530

    Chnge sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,10 +5,11 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schemaTypes } from "./sanity/schemas";
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+// import { apiVersion, dataset, projectId } from "./sanity/.env
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/sanity",
@@ -20,6 +21,6 @@ export default defineConfig({
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
+    visionTool(),
   ],
 });
